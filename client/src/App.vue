80db62d9a9e7da6e65f03b3bb3daa50ec5e6fa37
sontd <sontd@saisystem.vn>
2026-05-19 17:08:09 +0700

    client first commit

---
+++ client/src/App.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+  <h1 class="tw:text-primary!">You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+  <el-button type="primary">Hello Element Plus</el-button>
+</template>
+
+<style scoped></style>
